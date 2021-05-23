3a820d821034224a228264e86f389bdc0c58c881
Sitesh Pattanaik <12951458+spattk@users.noreply.github.com>
2021-05-23 16:09:03 +0530

    Time: 2 ms (98.24%), Space: 39 MB (57.14%) - LeetHub

--- queries-on-a-permutation-with-key/queries-on-a-permutation-with-key.java
+++ queries-on-a-permutation-with-key/queries-on-a-permutation-with-key.java
@@ -1,21 +1,49 @@
+import java.util.*;
+
 class Solution {
     public int[] processQueries(int[] queries, int m) {
-        ArrayList<Integer> p = new ArrayList<>();
-        for(int i=1;i<=m;i++){
-            p.add(i);
-        }
-        
         int n = queries.length;
-        
-        int [] result = new int[n];
-        for(int i=0;i<n;i++){
-            int temp = queries[i];
-            int idx = p.indexOf(temp);
-            result[i] = idx;
-            p.remove(new Integer(temp));
-            p.add(0, temp);
+        BIT bit = new BIT(n+m+1);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 1; i <= m; i++) {
+            bit.add(n+i, 1);
+            map.put(i, n+i);
         }
-        
-        return result;
+
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            int index = map.remove(queries[i]);
+            res[i] = bit.prefixSum(index-1);
+
+            int new_index = n - i;
+            bit.add(index, -1);
+            bit.add(new_index, 1);
+            map.put(queries[i], new_index);
+        }
+        return res;
+    }
+
+    class BIT {
+        int[] a;
+
+        public BIT(int n) {
+            a = new int[n];
+        }
+
+        public void add(int index, int delta) {
+            while (index < a.length) {
+                a[index] += delta;
+                index += index & (-index);
+            }
+        }
+
+        public int prefixSum(int index) {
+            int res = 0;
+            while (index > 0) {
+                res += a[index];
+                index -= index & (-index);
+            }
+            return res;
+        }
     }
 }

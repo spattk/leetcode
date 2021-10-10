205c2e25e76dae70f1d6eb57612517d9ecf47669
Sitesh Pattanaik <12951458+spattk@users.noreply.github.com>
2021-10-10 12:48:56 -0700

    Time: 0 ms (100.00%), Space: 38.8 MB (39.35%) - LeetHub

---
+++ merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -0,0 +1,69 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    
+    
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode node = null;
+        ListNode head = null;
+        if(l1 == null && l2 == null){
+            return node;
+        }
+        while(l1 != null && l2 != null){
+            if(l1.val <= l2.val){
+                if (node == null){
+                    node = new ListNode(l1.val);
+                    head = node;
+                } else {
+                    node.next = new ListNode(l1.val);
+                    node = node.next;
+                }
+                
+                l1 = l1.next;
+            } else {
+                if (node == null){
+                    node = new ListNode(l2.val);
+                    head = node;
+                } else {
+                    node.next = new ListNode(l2.val);
+                    node = node.next;
+                }
+                
+                l2 = l2.next;
+            }
+        }
+        
+        while( l1 != null){
+            if(node == null){
+                node = new ListNode(l1.val);
+                head = node;
+            } else {
+                node.next = new ListNode(l1.val);
+                node = node.next;
+            }
+            l1 = l1.next;
+        }
+        
+        while( l2 != null){
+            if(node == null){
+                node = new ListNode(l2.val);
+                head = node;
+            } else {
+                node.next = new ListNode(l2.val);
+                node = node.next;
+            }
+            
+            l2 = l2.next;
+        }
+        
+        return head;
+    }
+}

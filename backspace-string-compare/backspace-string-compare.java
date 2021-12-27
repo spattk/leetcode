372f7bb8e95caa7e3e466101f3654eb9c70712d1
Sitesh Pattanaik <12951458+spattk@users.noreply.github.com>
2021-12-26 17:22:40 -0800

    Time: 0 ms (100.00%), Space: 37.2 MB (76.43%) - LeetHub

---
+++ backspace-string-compare/backspace-string-compare.java
@@ -0,0 +1,81 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        int i = s.length() - 1;
+        int j = t.length() - 1;
+        
+        char ch1, ch2, ch;
+        int h1=0, h2=0;
+        
+        while(i>=0 && j >=0){
+            ch1 = s.charAt(i);
+            ch2 = t.charAt(j);
+            
+            if(ch1 != '#' && h1 >0){
+                h1--;
+                i--;
+                continue;
+            }
+            
+            if(ch2 != '#' && h2>0){
+                h2--;
+                j--;
+                continue;
+            }
+            
+            
+            if(ch1 != '#' && ch2 != '#'){
+                if(ch1 != ch2){
+                    if(h1>0 && h2>0){
+                        h1--;
+                        h2--;
+                    } else {
+                        return false;
+                    }
+                }
+                i--; j--;
+            } else if(ch1 == '#' && ch2 == '#') {
+                h1++;
+                h2++;
+                i--; j--;
+            } else if(ch1 == '#' && ch2 != '#') {
+                h1++;
+                i--;
+            } else if(ch1 != '#' && ch2 == '#') {
+                h2++;
+                j--;
+            }
+        }
+        
+        while(i>=0){
+            ch = s.charAt(i);
+            if(ch != '#') {
+                if(h1>0){
+                    h1--;
+                }
+                else{
+                    return false;
+                }
+            } else {
+                h1++;
+            }
+            i--;
+        }
+        
+        while(j>=0){
+            ch = t.charAt(j);
+            if(ch != '#'){
+                if(h2>0){
+                    h2--;
+                }
+                else{
+                    return false;
+                }
+            } else {
+                h2++;
+            }
+            j--;
+        }
+        
+        return true;
+    }
+}

54697e6784505530fee438798a894c06ab17087d
Sitesh Pattanaik <12951458+spattk@users.noreply.github.com>
2022-06-05 12:13:28 -0700

    Time: 16 ms (10.10%), Space: 40.3 MB (73.74%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.java
@@ -0,0 +1,72 @@
+class Solution {
+
+    
+    int[][] queens;
+    int count;
+    
+    public boolean isSafe(int row, int col, int n){
+        
+        if(row >= n || row < 0 || col >= n || col < 0)
+            return false;
+        
+        //check row
+        for(int j=0;j<n;j++){
+            if(j == col)
+                continue;
+            if(queens[row][j] == 1)
+                return false;
+        }
+        
+        //check col
+        for(int i=0;i<n;i++){
+            if(i == row)
+                continue;
+            if(queens[i][col] == 1)
+                return false;
+        }
+        
+        
+        //check diag
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(((i + j) == (row + col)) && queens[i][j] == 1){
+                    return false;
+                }
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(((i - j) == (row - col)) && queens[i][j] == 1){
+                    return false;
+                }
+            }
+        }
+        
+    
+        return true;
+    }
+    
+    public void solveNQueensUtil(int n, int row){
+        if(row == n){
+            count++;
+            return;
+        }
+        
+        for(int col= 0; col < n; col ++){
+            if(isSafe(row, col, n)){
+                queens[row][col] = 1;
+                solveNQueensUtil(n, row+1);
+                queens[row][col] = 0;
+            }
+        }
+        
+    }
+    
+    public int totalNQueens(int n)  {
+        queens = new int[n][n];
+        solveNQueensUtil(n, 0);
+        
+        return count;
+    }
+}

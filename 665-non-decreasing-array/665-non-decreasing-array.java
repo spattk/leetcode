9e55f2ca972d1544ec2282761d41287613266c6f
Sitesh Pattanaik <12951458+spattk@users.noreply.github.com>
2022-06-25 14:37:50 -0700

    Time: 25 ms (5.21%), Space: 53.9 MB (53.67%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.java
+++ 665-non-decreasing-array/665-non-decreasing-array.java
@@ -1,21 +1,29 @@
 class Solution {
     
     public int lengthOfLIS(int[] nums) {
-        int[] tails = new int[nums.length];
-        int size = 0;
-        for (int x : nums) {
-            int i = 0, j = size;
-            while (i != j) {
-                int m = (i + j) / 2;
-                if (tails[m] <= x)
-                    i = m + 1;
-                else
-                    j = m;
-            }
-            tails[i] = x;
-            if (i == size) ++size;
+        if(nums == null || nums.length == 0) return 0;
+        if(nums.length == 1) return 1;
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        for(int i = 0;i< dp.length;i++){
+            int pos = binarySearchPosition(dp,nums[i],i);
+            dp[pos] = nums[i];
         }
-        return size;
+        for(int i = dp.length - 1;i >= 0;i--){
+            if(dp[i] != Integer.MAX_VALUE) return i + 1;
+        }
+        return 0;
+    }
+
+
+    private int binarySearchPosition(int[] dp,int target,int hi){
+        int low = 0;
+        while(low <= hi){
+            int mid = low + (hi - low)/2;
+            if(target < dp[mid]) hi = mid - 1;
+            else if(target >= dp[mid]) low = mid + 1;
+        }
+        return low;
     }
     
     public boolean checkPossibility(int[] nums) {

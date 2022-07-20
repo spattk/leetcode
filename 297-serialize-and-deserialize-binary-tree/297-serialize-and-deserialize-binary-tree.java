8fd8a91e8b0cb06499a198f13c1dd75f7765eb95
Sitesh Pattanaik <12951458+spattk@users.noreply.github.com>
2022-07-19 23:54:29 -0700

    Time: 191 ms (13.67%), Space: 200.1 MB (12.92%) - LeetHub

---
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.java
@@ -0,0 +1,94 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+    
+    static int pos = 0;
+    static String treePath;
+    
+    public int extractNum(String data){
+        // System.out.println("-------");
+        int num = 0;
+        char ch = data.charAt(pos);
+        int sign = 1;
+        while(Character.isDigit(ch) || ch == '-'){
+            // System.out.println(ch);
+            if(ch == '-'){
+                sign = -1;
+            } else {
+                num *= 10;
+                num += (ch - '0');
+            }
+            pos++;
+            
+            if(pos == data.length())
+                break;
+            ch = data.charAt(pos);
+        }
+        // System.out.println(num);
+        // System.out.println("-------");
+        return num * sign;
+    }
+    
+    public TreeNode buildTree(String data){
+        if(pos == data.length() || data == null)
+            return null;
+        
+        if(data.charAt(pos) == '*'){
+            pos++;
+            return null;
+        }
+        
+        if(data.charAt(pos) == ','){
+            pos++;
+        }
+        
+        if(data.charAt(pos) == '*'){
+            pos++;
+            return null;
+        }
+        
+        int num = extractNum(data);
+        TreeNode root = new TreeNode(num);
+        root.left = buildTree(data);
+        root.right = buildTree(data);
+
+        return root;
+    }
+    
+    public void buildString(TreeNode root){
+        if(root == null){
+            treePath += "*";
+            return;
+        }
+        
+        treePath += root.val+ ",";
+        buildString(root.left);
+        buildString(root.right);
+    }
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        treePath = "";
+        buildString(root);
+        // System.out.println(treePath);
+        return treePath;
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        pos = 0;
+        return buildTree(data);
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// TreeNode ans = deser.deserialize(ser.serialize(root));

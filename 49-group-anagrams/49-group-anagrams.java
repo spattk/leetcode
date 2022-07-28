e66282b3f0079e5aacf277a61d65a63d07f8e41a
Sitesh Pattanaik <12951458+spattk@users.noreply.github.com>
2022-07-28 00:11:11 -0700

    Time: 719 ms (5.02%), Space: 56.9 MB (30.45%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.java
@@ -0,0 +1,75 @@
+class Solution {
+    
+    public void generateCharMap(int[][] charMap, String str, int i){
+        for(char ch: str.toCharArray())
+            charMap[i][ch-'a']++;
+    }
+    
+    public boolean isAnagram(int[] c1, int [] c2){
+        for(int i=0; i<26; i++){
+            if(c1[i] != c2[i])
+                return false;
+        }
+        
+        return true;
+    }
+    
+    public void generateList(List<List<String>> result, Map<Integer, Set<Integer>> map, int n, String[] strs){
+        
+        
+        Set<Integer> visited = new HashSet<>();
+        for(int key: map.keySet()){
+            visited.add(key);
+            List<String> temp = new ArrayList<>();
+            temp.add(strs[key]);
+            for(int ele: map.get(key)){
+                visited.add(ele);
+                temp.add(strs[ele]);
+            }
+            result.add(new ArrayList<>(temp));
+        }
+        
+        for(int i=0;i<n;i++){
+            if(!visited.contains(i)){
+                List<String> temp = new ArrayList<>();
+                temp.add(strs[i]);
+                result.add(new ArrayList<>(temp));
+            }
+        }
+        
+    }
+    
+    public List<List<String>> groupAnagrams(String[] strs) {
+        int n = strs.length;
+        int[][] charMap = new int[n][26];
+        for(int i=0; i<n; i++){
+            String str = strs[i];
+            generateCharMap(charMap, str, i);
+        }
+        
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        
+        int[] parent = new int[n];
+        for(int i=0; i<n ;i++)
+            parent[i] = i;
+        
+        for(int i=0; i<n; i++){
+            if(parent[i] != i)
+                continue;
+            
+            for(int j=i+1; j<n; j++){
+                if(isAnagram(charMap[i], charMap[j])){
+                    parent[j] = i;
+                    Set<Integer> set = map.getOrDefault(i, new HashSet<>());
+                    set.add(j);
+                    map.put(i, set);
+                }
+            }
+        }
+        
+        List<List<String>> result = new ArrayList<>();
+        generateList(result, map, n, strs);
+        return result;
+        
+    }
+}

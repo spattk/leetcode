040cfc49bb38d1b0f3ec39738e279eb81e826016
Sitesh Pattanaik <12951458+spattk@users.noreply.github.com>
2022-08-06 00:37:02 -0700

    Time: 0 ms (100.00%), Space: 40.8 MB (67.20%) - LeetHub

---
+++ 458-poor-pigs/458-poor-pigs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int pigs = 0;
+        while (Math.pow(minutesToTest / minutesToDie + 1, pigs) < buckets) {
+            pigs += 1;
+        }
+        return pigs;
+    }
+}
+
+/* 
+4 sets
+2 ^ 10 / 2 ^ 2
+
+*/

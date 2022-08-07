b6e9919093a18524872ef083f095a1b3b7732a9b
Sitesh Pattanaik <12951458+spattk@users.noreply.github.com>
2022-08-07 00:48:53 -0700

    Time: 131 ms (18.60%), Space: 47.5 MB (46.24%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -10,46 +10,37 @@
  */
 class Solution {
     
-    public ListNode mergeList(ListNode first, ListNode list){
-        ListNode result = new ListNode(0);
-        ListNode head = result;
-        while(first != null && list != null){
-            if(first.val <= list.val){
-                result.next = first;
-                first = first.next;
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode head = new ListNode(0);
+        ListNode temp = head;
+        
+        while(list1 != null && list2 != null){
+            if(list1.val <= list2.val){
+                temp.next = list1;
+                list1 = list1.next;
             } else {
-                result.next = list;
-                list = list.next;
+                temp.next = list2;
+                list2 = list2.next;
             }
-            result = result.next;
+            
+            temp = temp.next;
         }
         
+        if(list1 != null)
+            temp.next = list1;
         
-        if(first != null){
-            result.next = first;
-        }
+        if(list2 != null)
+            temp.next = list2;
         
-        if(list != null)
-            result.next = list;
-        
-       return head.next;
+        return head.next;
     }
     
     public ListNode mergeKLists(ListNode[] lists) {
-        int n = lists.length;
-        
-        if(n == 0)
-            return null;
-        
-        int i = 0;
-        ListNode result = lists[0];
-        ListNode resultHead = result;
-        
-        for(i=1; i<n; i++){
-            result = mergeList(result, lists[i]);
-            resultHead = result;
+        ListNode result = null;
+        for(ListNode list: lists){
+            result = mergeTwoLists(result, list);
         }
         
-        return resultHead;
+        return result;
     }
 }

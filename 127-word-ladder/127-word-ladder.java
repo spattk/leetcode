c8f8846945d53e73357cdd7d8ae02c2b20ba79b8
Sitesh Pattanaik <12951458+spattk@users.noreply.github.com>
2022-08-14 00:58:04 -0700

    Time: 157 ms (55.23%), Space: 90.8 MB (33.74%) - LeetHub

--- 127-word-ladder/127-word-ladder.java
+++ 127-word-ladder/127-word-ladder.java
@@ -47,8 +47,11 @@ class Solution {
     }
     
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Map<String, Set<String>> adj = new HashMap<>();
-        createAdjList(adj, beginWord, wordList);
+        
+        Set<String> words = new HashSet<>(wordList);
+        if(!words.contains(endWord))
+            return 0;
+        
         Set<String> visited = new HashSet<>();
         Queue<String> q = new LinkedList<>();
         q.add(beginWord);
@@ -65,10 +68,18 @@ class Solution {
                     minLevel = level;
                     return minLevel;
                 }
-                for(String str: adj.getOrDefault(curr, new HashSet<>())){
-                    if(!visited.contains(str)){
-                        visited.add(str);
-                        q.add(str);
+                char[] chars = curr.toCharArray();
+                char prev;
+                for(int pos=0; pos< chars.length; pos++){
+                    for(int idx= 0; idx<26;idx++){
+                        prev = chars[pos];
+                        chars[pos] = (char) ('a' + idx);
+                        String word = new String(chars);
+                        if(words.contains(word) && !visited.contains(word)){
+                            visited.add(word);
+                            q.add(new String(chars));
+                        }
+                        chars[pos] = prev;
                     }
                 }
             }

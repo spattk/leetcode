f7a8e0fee71926ac534f4c4d0066b9957264de28
Sitesh Pattanaik <12951458+spattk@users.noreply.github.com>
2022-08-20 12:26:02 -0700

    Time: 4 ms (86.38%), Space: 48.2 MB (84.59%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.java
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.java
@@ -1,33 +1,24 @@
 class Solution {
+    
     public int minRefuelStops(int target, int startFuel, int[][] stations) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((p,q)->q[1]-p[1]);
-        
-        Arrays.sort(stations, (p,q)->p[0]-q[0]);
         int n = stations.length;
-        int count = 0, curr = startFuel;
-        int i = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->b[1]-a[1]);
+        int refill = 0;int i=0;
+        int distance = startFuel;
         
-        while(true) {
-            if(curr >= target)
-                return count;
-            
-            while(i < n && stations[i][0] <= curr) {
-                pq.add(stations[i]);
+        while(distance < target){
+            while(i<n && distance >= stations[i][0]){
+                pq.offer(stations[i]);
                 i++;
             }
             
-            if(!pq.isEmpty()){
-                curr += pq.poll()[1];
-                count++;
-            } else {
-                break;
-            }
+            if (pq.isEmpty())
+                return -1;
+            
+            distance += pq.remove()[1];
+            refill++;
         }
         
-        if (curr >= target){
-            return count;
-        } else {
-            return -1;
-        }
+        return refill;
     }
 }

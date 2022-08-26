e7490e2e1a33e30b4f2eae06483a1d33309299c4
Sitesh Pattanaik <12951458+spattk@users.noreply.github.com>
2022-08-25 22:54:09 -0700

    Time: 6 ms (23.33%), Space: 41.8 MB (22.00%) - LeetHub

---
+++ 869-reordered-power-of-2/869-reordered-power-of-2.java
@@ -0,0 +1,36 @@
+class Solution {
+    
+    public boolean matches(int[] arr, int[] brr){
+    
+        for(int i=0; i<10;i++)
+            if(arr[i] != brr[i])
+                return false;
+        
+        
+        return true;
+        
+    }
+    
+    public int[] getMap(String s){
+        int[] map = new int[10];
+        for(char ch: s.toCharArray())
+            map[ch-'0']++;
+        
+        return map;
+    }
+    
+    Set<String> powerOf2 = new HashSet<>();
+    public boolean reorderedPowerOf2(int n) {
+        for(int i=0; i<31;i++){
+            powerOf2.add(new String("" + (int)Math.pow(2, i)));
+        }
+        
+        String num = "" + n;
+        for(String str: powerOf2){
+            if(matches(getMap(num), getMap(str)))
+                return true;
+        }
+        
+        return false;
+    }
+}

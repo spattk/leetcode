3f404c8de4856f31135dd3c35c24edafefd1b360
Sitesh Pattanaik <12951458+spattk@users.noreply.github.com>
2022-09-06 00:37:29 -0700

    Time: 6 ms (6.29%), Space: 40.8 MB (86.20%) - LeetHub

---
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(char ch: s.toCharArray()){
+            if(st.isEmpty()){
+                st.push(ch);
+            } else {
+                if(st.peek() == '(' && ch == ')') {
+                    st.pop();
+                } else {
+                    st.push(ch);
+                }
+            }
+        }
+        
+        return st.size();
+    }
+}

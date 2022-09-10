28d1dc8610afbad6c39d917f0bbf2da703fe1efd
Sitesh Pattanaik <12951458+spattk@users.noreply.github.com>
2022-09-09 19:11:09 -0700

    Time: 8 ms (46.23%), Space: 43.7 MB (18.40%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.java
+++ 10-regular-expression-matching/10-regular-expression-matching.java
@@ -1,25 +1,42 @@
 class Solution {
     
+    Map<Pair<Integer, Integer>, Boolean> map = new HashMap<>();
     public boolean isMatchUtil(String s, String p, int i, int j){
+        Pair<Integer, Integer> pair = new Pair(i, j);
+        if(map.get(pair) != null)
+            return map.get(pair);
+            
+        boolean result = false;
         if(i >= s.length() && j >= p.length()){
-            return true;
+            result = true;
+            map.put(pair, result);
+            return result;
         }
         
         if(j >= p.length()){
-            return false;
+            result = false;
+            map.put(pair, result);
+            return result;
         }
         
         boolean match = i < s.length() && ((s.charAt(i) == p.charAt(j) || p.charAt(j) == '.'));
         
         if((j+1) < p.length() && p.charAt(j+1) == '*'){
             //ignore it or take it
-            return isMatchUtil(s,p,i,j+2) || match && isMatchUtil(s,p,i+1,j);
+            result= isMatchUtil(s,p,i,j+2) || match && isMatchUtil(s,p,i+1,j);
+            map.put(pair, result);
+            return result;
         }
         
         if(match){
-            return isMatchUtil(s,p,i+1, j+1);
+            result =  isMatchUtil(s,p,i+1, j+1);
+            map.put(pair, result);
+            return result;
+            
         }
-        return false;
+        result= false;
+        map.put(pair, result);
+        return result;
     }
     
     public boolean isMatch(String s, String p) {

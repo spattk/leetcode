86808b79197a7c9b9b540be9a3b37f854ac0677f
Sitesh Pattanaik <12951458+spattk@users.noreply.github.com>
2022-09-14 18:45:34 -0700

    Time: 284 ms (30.54%), Space: 150.7 MB (16.02%) - LeetHub

---
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int[] findOriginalArray(int[] changed) {
+        Arrays.sort(changed);
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int num: changed){
+            map.put(num, map.getOrDefault(num, 0)+1);
+        }
+        
+        List<Integer> result = new ArrayList<>();
+        for(int num: changed){
+            
+            if(num == 0){
+                if(map.getOrDefault(num, 0) >= 2){
+                    result.add(num);
+                    map.put(num, map.get(num)-2);
+                } else if(map.getOrDefault(num, 0) == 1) {
+                    return new int[]{};
+                } else if(map.getOrDefault(num, 0) == 0){
+                    continue;
+                }
+            }
+            
+            else if(map.getOrDefault(num, 0) != 0){
+                if(map.getOrDefault(2 * num, 0) != 0){
+                    result.add(num);
+                    map.put(num, map.get(num)-1);
+                    map.put(num *2, map.get(2*num)-1);
+                } else {
+                    //double is absent
+                    return new int[]{};
+                }
+            }
+        }
+        
+        return result.stream().mapToInt(Integer::intValue).toArray();
+    }
+}

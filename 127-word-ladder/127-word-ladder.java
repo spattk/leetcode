7eeb2b015de5309af6263b3f257d487ba14c062a
Sitesh Pattanaik <12951458+spattk@users.noreply.github.com>
2022-09-18 13:30:52 -0700

    Time: 98 ms (79.77%), Space: 45.1 MB (89.05%) - LeetHub

--- 127-word-ladder/127-word-ladder.java
+++ 127-word-ladder/127-word-ladder.java
@@ -2,49 +2,49 @@ class Solution {
     
     int minLevel = 0;
     
-    public boolean isAdj(String s1, String s2){
-        if(s1.length() != s2.length())
-            return false;
+//     public boolean isAdj(String s1, String s2){
+//         if(s1.length() != s2.length())
+//             return false;
         
-        int count = 0;
-        int n = s1.length();
-        for(int i=0; i<n; i++){
-            if(s1.charAt(i) != s2.charAt(i)){
-                count++;
+//         int count = 0;
+//         int n = s1.length();
+//         for(int i=0; i<n; i++){
+//             if(s1.charAt(i) != s2.charAt(i)){
+//                 count++;
                 
-                if(count > 1)
-                    return false;
-            }
-        }
+//                 if(count > 1)
+//                     return false;
+//             }
+//         }
         
-        return count == 1;
-    }
+//         return count == 1;
+//     }
     
-    public void createAdjList(Map<String, Set<String>> adj, String beginWord, List<String> wordList) {
-        int n = wordList.size();
-        for(int i=0; i< n; i++){
-            for(int j=0; j< n; j++){
-                if(i == j)
-                    continue;
+//     public void createAdjList(Map<String, Set<String>> adj, String beginWord, List<String> wordList) {
+//         int n = wordList.size();
+//         for(int i=0; i< n; i++){
+//             for(int j=0; j< n; j++){
+//                 if(i == j)
+//                     continue;
                 
-                if(isAdj(wordList.get(i), wordList.get(j))){
-                    Set<String> set = adj.getOrDefault(wordList.get(i), new HashSet<>());
-                    set.add(wordList.get(j));
-                    adj.put(wordList.get(i), set);
-                }
-            }
-        }
+//                 if(isAdj(wordList.get(i), wordList.get(j))){
+//                     Set<String> set = adj.getOrDefault(wordList.get(i), new HashSet<>());
+//                     set.add(wordList.get(j));
+//                     adj.put(wordList.get(i), set);
+//                 }
+//             }
+//         }
         
-        if(adj.get(beginWord) == null){
-            for(int i=0;i<n;i++){
-                if(isAdj(wordList.get(i), beginWord)){
-                    Set<String> set = adj.getOrDefault(beginWord, new HashSet<>());
-                    set.add(wordList.get(i));
-                    adj.put(beginWord, set);
-                }
-            }
-        }
-    }
+//         if(adj.get(beginWord) == null){
+//             for(int i=0;i<n;i++){
+//                 if(isAdj(wordList.get(i), beginWord)){
+//                     Set<String> set = adj.getOrDefault(beginWord, new HashSet<>());
+//                     set.add(wordList.get(i));
+//                     adj.put(beginWord, set);
+//                 }
+//             }
+//         }
+//     }
     
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         

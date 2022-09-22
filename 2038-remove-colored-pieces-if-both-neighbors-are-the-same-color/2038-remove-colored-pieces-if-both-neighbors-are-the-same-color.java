c140270dbd66f925009829a2276d6120241a5e6d
Sitesh Pattanaik <12951458+spattk@users.noreply.github.com>
2022-09-22 10:51:00 -0700

    Time: 25 ms (50.82%), Space: 54 MB (69.84%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
@@ -1,16 +1,41 @@
 class Solution {
+    
+    public boolean isATriplet(char first, char second, char third){
+        return first == 'A' && second == 'A' && third == 'A';
+    }
+    
+    public boolean isBTriplet(char first, char second, char third){
+        return first == 'B' && second == 'B' && third == 'B';
+    }
+    
     public boolean winnerOfGame(String colors) {
         int aTriplets = 0, bTriplets = 0;
-        for(int i=0; i<=colors.length() - 3; i++){
-            String str = colors.substring(i, i+3);
-            if(str.equals("AAA")){
+        char first = '0', second = '0', third = '0';
+        if(colors.length() >=3  ) {
+            first = colors.charAt(0);
+            second = colors.charAt(1);
+            third = colors.charAt(2);
+        }
+        
+        if(isATriplet(first, second, third)){
+            aTriplets++;
+        } else if (isBTriplet(first, second, third)) {
+            bTriplets++;
+        }
+        
+        for(int i=1; i<=colors.length() - 3; i++){
+            first = second;
+            second = third;
+            third = colors.charAt(i+2);
+            
+            if(isATriplet(first, second, third)){
                 aTriplets++;
-            } else if (str.equals("BBB")) {
+            } else if (isBTriplet(first, second, third)) {
                 bTriplets++;
             }
         }
         
-        // System.out.println(aTriplets + " " + bTriplets);
+        
         if(aTriplets > bTriplets)
             return true;
         return false;

8555595a7e37162048da54dead8b00a561a7af06
Sitesh Pattanaik <12951458+spattk@users.noreply.github.com>
2022-09-25 16:24:34 -0700

    Time: 19 ms (7.29%), Space: 42.6 MB (29.16%) - LeetHub

---
+++ 409-longest-palindrome/409-longest-palindrome.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(char ch: s.toCharArray()){
+            map.put(ch, map.getOrDefault(ch,0)+1);
+        }
+        
+        System.out.println(map);
+        
+        int total = 0;
+        boolean odd = false;
+        for(char key: map.keySet()){
+            if(map.get(key) % 2== 0){
+                total += map.get(key);
+            } else {
+                total += map.get(key) - 1;
+                odd = true;
+            }
+        }
+        
+        return odd== true? total + 1 : total;
+    }
+}

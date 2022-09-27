765b55d90f049333b24d3d19cf338fd5e9a55ea8
Sitesh Pattanaik <12951458+spattk@users.noreply.github.com>
2022-09-27 01:16:58 -0700

    Time: 2 ms (64.64%), Space: 42.2 MB (27.88%) - LeetHub

---
+++ 362-design-hit-counter/362-design-hit-counter.java
@@ -0,0 +1,27 @@
+class HitCounter {
+
+    PriorityQueue<Integer> pq;
+    public HitCounter() {
+        pq = new PriorityQueue<>();
+    }
+    
+    public void hit(int timestamp) {
+        pq.add(timestamp);
+    }
+    
+    public int getHits(int timestamp) {
+        
+        while(!pq.isEmpty() && pq.peek() <= (timestamp - 300)){
+            pq.poll();
+        }
+        
+        return pq.size();
+    }
+}
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter obj = new HitCounter();
+ * obj.hit(timestamp);
+ * int param_2 = obj.getHits(timestamp);
+ */

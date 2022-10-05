51d5c813991ce7e075c097a9e4eea1ae8d3345fd
Sitesh Pattanaik <12951458+spattk@users.noreply.github.com>
2022-10-05 15:16:24 -0700

    Time: 18 ms (29.36%), Space: 44 MB (59.66%) - LeetHub

---
+++ 139-word-break/139-word-break.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        boolean[] dp = new boolean[n+1];
+        dp[n] = true;
+        
+        for(int i=n-1; i>=0; i--){
+            for(String word: wordDict){
+                int len = word.length();
+                if((i + len) <= n && s.substring(i, i+len).equals(word)){
+                    dp[i] |= dp[i+len];
+                }
+            }
+        }
+        
+        return dp[0];
+    }
+}

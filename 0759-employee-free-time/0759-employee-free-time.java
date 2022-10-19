2dcd3891622ff10816d658794a14cbf8a1422872
Sitesh Pattanaik <12951458+spattk@users.noreply.github.com>
2022-10-19 16:47:42 -0700

    Time: 25 ms (50.79%), Space: 43.7 MB (96.13%) - LeetHub

---
+++ 0759-employee-free-time/0759-employee-free-time.java
@@ -0,0 +1,53 @@
+/*
+// Definition for an Interval.
+class Interval {
+    public int start;
+    public int end;
+
+    public Interval() {}
+
+    public Interval(int _start, int _end) {
+        start = _start;
+        end = _end;
+    }
+};
+*/
+
+class Solution {
+    public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for(List<Interval> eles: schedule){
+            for(Interval ele: eles){
+                map.put(ele.start, map.getOrDefault(ele.start, 0) + 1);
+                map.put(ele.end, map.getOrDefault(ele.end, 0) - 1);
+            }
+            
+        }
+        
+        int sum = 0;
+        Integer start = null;
+        List<Interval> list = new ArrayList<>();
+        Integer lastKey = null;
+        for(int key: map.keySet()){
+            sum += map.get(key);
+            
+            
+            if(sum == 0){
+                start = key;
+            }
+            
+            else if(start != null && sum != 0){
+                list.add(new Interval(start, key));
+                start = null;
+            }
+            
+            lastKey = key;
+        }
+        
+        if(start != null && !start.equals(lastKey)){
+            list.add(new Interval(start, lastKey));
+        }
+        
+        return list;
+    }
+}

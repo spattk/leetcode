971eb6baa902cd0eb4e4fcac37b20e1a5358ea2c
Sitesh Pattanaik <12951458+spattk@users.noreply.github.com>
2022-10-24 00:44:41 -0700

    Time: 142 ms (28.14%), Space: 42.2 MB (87.59%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.java
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.java
@@ -1,37 +1,65 @@
 class Solution {
-    
+     	
     Map<Integer, Integer> dp;
+	public static int compare(int[] p, int q[]){
+		int ret = Integer.compare(p[1],q[1]);
+		if(ret != 0)
+			return ret;
+
+		return Integer.compare(p[0], q[0]);
+	}
         
 	public int getSteps(int num){
 		
-        if(num == 1)
-            return 0;
+		Queue<int[]> q = new LinkedList<>();
+		q.add(new int[]{num, 0});
+		Set<Integer> visited = new HashSet<>();
+		visited.add(num);
+		while(!q.isEmpty()){
+			int[] curr = q.poll();
         
-        if(dp.get(num) != null){
-            return dp.get(num);
-        }
-		
-        int result;
-        
-        if(num % 2 == 0){
-            result =  1 + getSteps(num/2);
-        } else {
-            result = 1 + getSteps(3*num + 1);
             
-        }
+			if(curr[0] == 1) {
+				return curr[1];
+            }
             
-        dp.put(num, result);
-		return result;
+            if(dp.get(curr[0]) != null){
+                return curr[1] + dp.get(curr[0]);
+            }
+                
+                
+			int temp = curr[0];
+			int count = curr[1];
+
+			if(temp % 2 == 0){
+				if(!visited.contains(temp/2)){
+					visited.add(temp/2);
+					q.add(new int[]{temp/2, count + 1});
+				}
+			}
+
+			else {
+				if(!visited.contains(temp * 3 + 1)){
+					visited.add(temp*3 + 1);
+					q.add(new int[]{temp * 3 + 1, count + 1});
+				}
+
+			}
+		}
+
+		return -1;
 	}
 	
     public int getKth(int lo, int hi, int k) {
         dp = new HashMap<>();
      	List<int[]> list = new ArrayList<>();
 		for(int i=lo; i<=hi; i++){
-			list.add(new int[]{i, getSteps(i)});
+            int steps = getSteps(i);
+            dp.put(i, steps);
+			list.add(new int[]{i, steps});
 		}   
-    
-		Collections.sort(list, (a,b)-> b[1]!=a[1] ?a[1]-b[1]:a[0]-b[0]);
+
+		Collections.sort(list, Solution::compare);
 		return list.get(k-1)[0];
     }
 }

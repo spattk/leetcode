15196893562c81ba2391b6ad3e30c25092b0bc42
Sitesh Pattanaik <12951458+spattk@users.noreply.github.com>
2022-10-28 18:27:07 -0700

    Time: 44 ms (74.60%), Space: 42.9 MB (72.42%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.java
+++ 0835-image-overlap/0835-image-overlap.java
@@ -1,26 +1,31 @@
 class Solution {
-
-    protected int convolute(int[][] A, int[][] kernel, int xShift, int yShift) {
-        int result = 0;
-        for (int row = 0; row < A.length; ++row)
-            for (int col = 0; col < A.length; ++col)
-                result += A[row][col] * kernel[row + yShift][col + xShift];
-        return result;
+    /**
+     *  Shift the matrix M in up-left and up-right directions 
+     *    and count the ones in the overlapping zone.
+     */
+    protected int shiftAndCount(int xShift, int yShift, int[][] M, int[][] R) {
+        int leftShiftCount = 0, rightShiftCount = 0;
+        // count the cells of ones in the overlapping zone.
+        for (int mRow = yShift, rRow = 0; mRow < M.length; ++mRow, ++rRow) {
+            for (int mCol = xShift, rCol =0; mCol < M.length; ++mCol, ++rCol) {
+                if (M[mRow][mCol] == 1 && M[mRow][mCol] == R[rRow][rCol])
+                    leftShiftCount += 1;
+                if (M[mRow][rCol] == 1 && M[mRow][rCol] == R[rRow][mCol])
+                    rightShiftCount += 1;
+            }
+        }
+        return Math.max(leftShiftCount, rightShiftCount);
     }
 
     public int largestOverlap(int[][] A, int[][] B) {
-
-        int N = A.length;
-        int[][] B_padded = new int[3 * N - 2][3 * N - 2];
-        for (int row = 0; row < N; ++row)
-            for (int col = 0; col < N; ++col)
-                B_padded[row + N - 1][col + N - 1] = B[row][col];
-
         int maxOverlaps = 0;
-        for (int xShift = 0; xShift < 2 * N - 1; ++xShift)
-            for (int yShift = 0; yShift < 2 * N - 1; ++yShift) {
-                maxOverlaps = Math.max(maxOverlaps,
-                    convolute(A, B_padded, xShift, yShift));
+
+        for (int yShift = 0; yShift < A.length; ++yShift)
+            for (int xShift = 0; xShift < A.length; ++xShift) {
+                // move the matrix A to the up-right and up-left directions.
+                maxOverlaps = Math.max(maxOverlaps, shiftAndCount(xShift, yShift, A, B));
+                // move the matrix B to the up-right and up-left directions, which is equivalent to moving A to the down-right and down-left directions 
+                maxOverlaps = Math.max(maxOverlaps, shiftAndCount(xShift, yShift, B, A));
             }
 
         return maxOverlaps;

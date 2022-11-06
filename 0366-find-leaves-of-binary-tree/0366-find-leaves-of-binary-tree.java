2685e2558bf0c096e9ec25448db8ab8847b85adc
Sitesh Pattanaik <12951458+spattk@users.noreply.github.com>
2022-11-05 19:08:50 -0700

    Time: 3 ms (10.13%), Space: 42.6 MB (40.05%) - LeetHub

--- 0366-find-leaves-of-binary-tree/0366-find-leaves-of-binary-tree.java
+++ 0366-find-leaves-of-binary-tree/0366-find-leaves-of-binary-tree.java
@@ -1,62 +1,36 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    
-    List<List<Integer>> result = new ArrayList<>();
-    
-    public int findLeavesUtil(TreeNode root, List<Integer> curr){
-        if(root == null){
-            return 0;
-        }
-        
-        if(root.left == null && root.right == null){
-            curr.add(root.val);
-            return 1;
-        }
-        
-        int left = findLeavesUtil(root.left, curr);
-        int right = findLeavesUtil(root.right, curr);
-        
-        if(left == 1){
-            root.left = null;
-        }
-        
-        if(right == 1){
-            root.right = null;
-        }
-        
-        return 0;
-    }
-    
-    public boolean isLeaf(TreeNode root){
-        if(root == null)
-            return false;
-        
-        return root.left == null && root.right == null;
-    }
-    
-    public List<List<Integer>> findLeaves(TreeNode root) {
-        while(root != null && !isLeaf(root)){
-            List<Integer> temp = new ArrayList<>();
-            findLeavesUtil(root, temp);
-            result.add(temp);
-        }
-        
-        result.add(Arrays.asList(root.val));
-        
-        return result;
-    }
+	Map<Integer, List<Integer>> heightMap;
+
+	public int calculateHeightMap(TreeNode root){
+		if(root == null){
+			return -1;
+		}
+
+		int lh = calculateHeightMap(root.left);
+		int rh = calculateHeightMap(root.right);
+
+		int h = Math.max(lh, rh) + 1;
+
+		heightMap.putIfAbsent(h, new ArrayList<>());
+		heightMap.get(h).add(root.val);
+
+		return h;
+
+	}
+
+    	public List<List<Integer>> findLeaves(TreeNode root) {
+     		heightMap = new HashMap<>();
+		calculateHeightMap(root);
+
+		List<Integer> keys = new ArrayList<>(heightMap.keySet());
+		Collections.sort(keys);
+
+		List<List<Integer>> answer = new ArrayList<>();
+		for(int key: keys){
+			answer.add(heightMap.get(key));
+		}
+
+		return answer;
+    	}
 }
+

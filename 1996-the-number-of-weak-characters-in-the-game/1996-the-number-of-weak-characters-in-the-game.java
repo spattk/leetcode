3b10ad068f166d9fda27066dd337ab694bd82c32
Sitesh Pattanaik <12951458+spattk@users.noreply.github.com>
2022-11-08 12:45:07 -0800

    Time: 247 ms (13.63%), Space: 219.8 MB (5.02%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
@@ -1,32 +1,38 @@
 class Solution {
-    public int numberOfWeakCharacters(int[][] properties) {
-        //attack -> list(defense)
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        int maxA = 0, minA = (int) 1e5 + 1;
-        for(int[] ele: properties) {
-            map.putIfAbsent(ele[0], new ArrayList<>());
-            map.get(ele[0]).add(ele[1]);
-            maxA = Math.max(maxA, ele[0]);
-            minA = Math.min(minA, ele[0]);
-        }
-        
-        int maxD = -1;
-        int count = 0;
-        for(int i=maxA; i>=minA; i--) {
-            if(map.get(i) == null)
-                continue;
-            
-            for(int def: map.get(i)){
-                if(def < maxD)
-                    count++;
-            }
-            
-            for(int def: map.get(i)){
-                maxD = Math.max(maxD, def);
-            }
-            
-        }
-        
-        return count;
-    }
+	//defense to list of pairs
+	Map<Integer, List<int[]>> map;
+	int maxD = -1, maxA = -1, count = 0;
+	
+	public void fillMap(int[][] properties){
+		for(int []p: properties){
+			map.putIfAbsent(p[1], new ArrayList<>());
+			map.get(p[1]).add(p);
+			maxD = Math.max(maxD, p[1]);
+		}
+	}
+
+	public void processFromMaxD(){
+		while(maxD >= 0){
+			if(map.get(maxD) != null){
+				int currMax = -1;
+				for(int[] ele: map.get(maxD)){
+					if(maxA != -1 && ele[0] < maxA){
+						count++;
+					}
+					currMax = Math.max(currMax, ele[0]);
+				}
+
+				maxA = Math.max(maxA, currMax);
+			}
+			maxD--;
+		}
+	}
+
+    	public int numberOfWeakCharacters(int[][] properties) {
+     		map = new HashMap<>();
+		fillMap(properties);
+		processFromMaxD();
+		return count;
+    	}
 }
+

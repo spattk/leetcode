ee78b6f417e95a0251eeb7cecefceba1d401378b
Sitesh Pattanaik <12951458+spattk@users.noreply.github.com>
2022-11-08 16:12:46 -0800

    Time: 2 ms (41.21%), Space: 39.9 MB (87.26%) - LeetHub

---
+++ 0980-unique-paths-iii/0980-unique-paths-iii.java
@@ -0,0 +1,62 @@
+class Solution {
+	boolean[][] visited;
+	int r,c;
+	int[] start;
+	int[][] dir = {{-1,0}, {0, -1}, {0, 1}, {1, 0}};
+    int ans = 0;
+    int empty = 0;
+	
+	//figures the start and the end
+	public void init(int[][] grid){
+		for(int i=0; i<r; i++){
+			for(int j=0; j<c; j++){
+				if(grid[i][j] == 1) start = new int[]{i,j};
+                
+                if(grid[i][j] == 0) empty++;
+			}
+		}
+	}
+
+	public boolean isSafe(int x, int y){
+		return x>=0 && x<r && y >=0 && y<c;
+	}
+
+	public int pathCounter(int x, int y, int [][] grid, boolean[][] visited, int remain){
+
+        if(!isSafe(x, y) || visited[x][y] || grid[x][y] == -1 || grid[x][y] == 1)
+            return 0;
+		
+		if(grid[x][y] == 2 && remain == 0){
+            return 1;
+        }
+        
+        if(remain == 0)
+            return 0;
+
+        visited[x][y] = true;
+        
+        int count = 0;
+		for(int i=0; i<dir.length; i++){
+			int nx = x + dir[i][0];
+			int ny = y + dir[i][1];
+                
+            count += pathCounter(nx, ny, grid, visited, remain - 1);
+		}
+        
+        visited[x][y] = false;
+        return count;
+	}
+
+    public int uniquePathsIII(int[][] grid) {
+        r = grid.length;
+		c = grid[0].length;
+		visited = new boolean[r][c];
+		init(grid);
+		return pathCounter(start[0], start[1] + 1, grid, visited, empty) +
+            pathCounter(start[0] + 1, start[1], grid, visited, empty) +
+            pathCounter(start[0]- 1, start[1], grid, visited, empty) + 
+            pathCounter(start[0], start[1] -1, grid, visited, empty);
+    }
+}
+
+

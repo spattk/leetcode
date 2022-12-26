af475ee753d3341747f9b1c0288784d43607fe70
Sitesh Pattanaik <12951458+spattk@users.noreply.github.com>
2022-12-26 08:03:37 -0800

    Time: 2 ms (92.42%), Space: 42.5 MB (98.45%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,32 +1,11 @@
 class Solution {
     
-    HashMap<Integer, Boolean> map;
-    public boolean helper(int[] nums, int t){
-        if (t >= nums.length - 1){
-            return true;
-        }
-        
-        if (nums[t] == 0)
-            return false;
-        
-        if (t + nums[t] >= nums.length - 1){
-            return true;
-        }
-        
-        if(map.get(t) != null){
-            return map.get(t);
-        }
-        
-        boolean result = false;
-        for(int i=t+1;i<=t+nums[t];i++){
-            result |= helper(nums, i);
-        }
-        map.put(t, result);
-        return result;
-    }
-    
     public boolean canJump(int[] nums) {
-        map = new HashMap<>();
-        return helper(nums, 0);
+    int reachable = 0;
+    for (int i=0; i<nums.length; ++i) {
+        if (i > reachable) return false;
+        reachable = Math.max(reachable, i + nums[i]);
+    }
+    return true;
     }
 }

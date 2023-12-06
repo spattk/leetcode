5e0c56f5d7c306996a318bcdf4205e93a5382eab
Sitesh Pattanaik <12951458+spattk@users.noreply.github.com>
2023-12-05 23:01:23 -0800

    Time: 0 ms (100.00%), Space: 41.6 MB (71.53%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -2,7 +2,6 @@ class Solution {
     
     List<List<Integer>> result = new ArrayList<>();
     List<Integer> currentState = new ArrayList<>();
-    Set<Integer> visited = new HashSet<>();
     
     public void solve(int[] nums, int idx) {
     
@@ -10,15 +9,9 @@ class Solution {
         result.add(new ArrayList<>(currentState));
         
         for(int i=idx; i<nums.length; i++) {
-            
-            if(visited.contains(nums[idx]))
-                return;
-            
-            visited.add(nums[idx]);
             currentState.add(nums[i]);
             solve(nums, i+1);
             currentState.remove(currentState.size() - 1);
-            visited.remove(nums[idx]);
         }
         
         

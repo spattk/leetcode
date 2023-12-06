715ded29535cc5b459b4728774fac9c21a7c5ff6
Sitesh Pattanaik <12951458+spattk@users.noreply.github.com>
2023-12-05 23:46:19 -0800

    Time: 2 ms (41.79%), Space: 44.3 MB (10.41%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,35 +1,33 @@
 class Solution {
     
-    Set<Integer> visited = new HashSet<>();
     List<Integer> currentState = new ArrayList<>();
-    
     List<List<Integer>> result = new ArrayList<>();
+    Set<Integer> visited = new HashSet<>();
     
-    public void solve(int[] nums, int ele) {
-        if(currentState.size() == nums.length - 1){
-            //base condition
-            currentState.add(ele);
+    public void solve(int[] nums) {
+        
+        if(currentState.size() == nums.length){
             result.add(new ArrayList<>(currentState));
-            currentState.remove(currentState.size() - 1);
             return;
         }
         
-        visited.add(ele);
-        currentState.add(ele);
         for(int idx = 0; idx< nums.length; idx++){
-            if(!visited.contains(nums[idx])){
-                solve(nums, nums[idx]);
+            
+            if(!visited.contains(nums[idx])) {
+                visited.add(nums[idx]);
+                currentState.add(nums[idx]);
+                solve(nums);
+                currentState.remove(currentState.size() - 1);
+                visited.remove(nums[idx]);
             }
+            
+            
         }
-        currentState.remove(currentState.size() - 1);
-        visited.remove(ele);
     }
     
     public List<List<Integer>> permute(int[] nums) {
         
-        for(int idx =0; idx< nums.length; idx++){
-            solve(nums, nums[idx]);
-        }
+        solve(nums);
         
         return result;
     }
